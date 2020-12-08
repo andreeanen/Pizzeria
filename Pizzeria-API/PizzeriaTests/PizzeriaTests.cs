a3372e0b5c5316826be60e9338ca7ffc9f481522
andreeanen <andreeanen@yahoo.com>
2020-12-08 09:54:44 +0100

    Setup project

---
+++ Pizzeria-API/PizzeriaTests/PizzeriaTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PizzeriaTests
+{
+    [TestClass]
+    public class PizzeriaTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
