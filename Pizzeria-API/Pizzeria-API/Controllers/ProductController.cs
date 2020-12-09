41f320ad6fc9b46135d9ca6c2b7ef804e1cc8a3c
HubTicaBB <tijana.ilic87@protonmail.com>
2020-12-09 18:33:33 +0100

    Create tests for Get menu http request

--- Pizzeria-API/Pizzeria-API/Controllers/ProductController.cs
+++ Pizzeria-API/Pizzeria-API/Controllers/ProductController.cs
@@ -1,68 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using Pizzeria_API.Data;
-using Pizzeria_API.Data.Factory;
 using Pizzeria_API.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Pizzeria_API.Controllers
 {
-
     [Route("api/[controller]")]
     [ApiController]
     public class ProductController : ControllerBase
     {
         private Database _database;
-        private Menu _menu;
+
+        public Menu Menu { get; set; }
 
         public ProductController()
         {
             _database = new Database();
-            _menu = _database.InitializeMenu();
+            Menu = _database.InitializeMenu();
         }
 
         //// GET: api/<ProductController>
         [HttpGet]
         public IActionResult Get()
         {
-            var products = _menu;
+            var products = Menu;
             if (products == null)
             {
                 return NotFound();
             }
           
             return Ok(products);
-
-        }
-       
-        // GET api/<ProductController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<ProductController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<ProductController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ProductController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
--- Pizzeria-API/PizzeriaTests/PizzeriaTests.cs
+++ Pizzeria-API/PizzeriaTests/PizzeriaTests.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pizzeria_API.Controllers;
+using Pizzeria_API.Data;
+using Pizzeria_API.Models;
 
 namespace PizzeriaTests
 {
@@ -6,8 +10,25 @@ namespace PizzeriaTests
     public class PizzeriaTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Get_MenuIsNull_ReturnsNotFoundResult()
         {
+            Menu menu = null;
+            var controller = new ProductController();
+            controller.Menu = menu;
+
+            var actual = controller.Get();
+
+            Assert.IsInstanceOfType(actual, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void Get_MenuHasProducts_ReturnsOk()
+        {
+            var controller = new ProductController();
+
+            var actual = controller.Get();
+
+            Assert.IsInstanceOfType(actual, typeof(OkObjectResult));
         }
     }
 }
